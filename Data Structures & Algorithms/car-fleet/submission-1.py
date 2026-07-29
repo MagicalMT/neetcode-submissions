d7873d805697433644b62e3a544fff031146dbb0
SimonXiong <79557305+MagicalMT@users.noreply.github.com>
2026-07-28 18:25:39 -0700

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+        order = list(zip(position, speed))
+        order = sorted(order, reverse = True)
+        for i in range(len(order)):
+            new_time = (target - order[i][0]) / order[i][1]
+            if not stack:
+                stack.append(new_time)
+                continue
+            current_time = stack[-1]
+            if new_time > current_time:
+                stack.append(new_time)
+        print(stack)
+        return len(stack)
+                
